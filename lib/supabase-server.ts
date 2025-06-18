2c6b641e5ecea0d9897061e35004d75cfb489de7
Alex Rendler <126621758+alexrendlerCS@users.noreply.github.com>
2025-06-18 10:38:58 -0700

    Cookies Compliance Update

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,25 +1,45 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { Database } from "./database.types";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 export function createClient() {
-  const cookieStore = cookies();
-
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
+        async get(name: string) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
+            const cookieStore = await cookies();
+            return cookieStore.get(name)?.value;
           } catch {
-            // The `setAll` method was called from a Server Component.
+            return undefined;
+          }
+        },
+        async set(
+          name: string,
+          value: string,
+          options: Omit<ResponseCookie, "name" | "value">
+        ) {
+          try {
+            const cookieStore = await cookies();
+            cookieStore.set({ name, value, ...options });
+          } catch {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        async remove(
+          name: string,
+          options: Omit<ResponseCookie, "name" | "value">
+        ) {
+          try {
+            const cookieStore = await cookies();
+            cookieStore.delete(name);
+          } catch {
+            // The `remove` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
@@ -34,16 +54,37 @@ export const supabaseAdmin = createServerClient<Database>(
   process.env.SUPABASE_SERVICE_ROLE_KEY!, // use service role for server-only mutations
   {
     cookies: {
-      getAll() {
-        return cookies().getAll();
-      },
-      setAll(cookiesToSet) {
+      async get(name: string) {
         try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookies().set(name, value, options)
-          );
+          const cookieStore = await cookies();
+          return cookieStore.get(name)?.value;
         } catch {
-          // The `setAll` method was called from a Server Component.
+          return undefined;
+        }
+      },
+      async set(
+        name: string,
+        value: string,
+        options: Omit<ResponseCookie, "name" | "value">
+      ) {
+        try {
+          const cookieStore = await cookies();
+          cookieStore.set({ name, value, ...options });
+        } catch {
+          // The `set` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
+      },
+      async remove(
+        name: string,
+        options: Omit<ResponseCookie, "name" | "value">
+      ) {
+        try {
+          const cookieStore = await cookies();
+          cookieStore.delete(name);
+        } catch {
+          // The `remove` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
           // user sessions.
         }
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
-  })
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -14,47 +14,47 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         get(name: string) {
-          return request.cookies.get(name)?.value
+          return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: any) {
           request.cookies.set({
             name,
             value,
             ...options,
-          })
+          });
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
-          })
+          });
           response.cookies.set({
             name,
             value,
             ...options,
-          })
+          });
         },
         remove(name: string, options: any) {
           request.cookies.delete({
             name,
             ...options,
-          })
+          });
           response = NextResponse.next({
             request: {
               headers: request.headers,
             },
-          })
+          });
           response.cookies.delete({
             name,
             ...options,
-          })
+          });
         },
       },
     }
-  )
+  );
 
-  await supabase.auth.getUser()
+  await supabase.auth.getUser();
 
-  return response
+  return response;
 }
 
 export const config = {
@@ -66,6 +66,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-} 
+};
